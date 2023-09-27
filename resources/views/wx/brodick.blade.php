d92dcb37992447bebf4ee03233caa62fd9b03dab
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2023-09-27 15:53:59 +0100

    Update brodick.blade.php

--- resources/views/wx/brodick.blade.php
+++ resources/views/wx/brodick.blade.php
@@ -217,7 +217,7 @@ echo ' <td class="text-dark">'.$wind. ' mph</td>';
 echo ' <td class="text-dark"> <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 512 512" transform="rotate('.$rotate.')"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 0a256 256 0 1 0 0 512A256 256 0 1 0 256 0zM127 281c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l71 71L232 136c0-13.3 10.7-24 24-24s24 10.7 24 24l0 182.1 71-71c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9L273 393c-9.4 9.4-24.6 9.4-33.9 0L127 281z"/></svg> </td>';
 echo ' <td class="text-dark"> '.$dirn. '</td>';
 echo ' <td class="text-dark"> '.$rain.'mm</td>';
-echo ' <td class="text-dark"> '.($data['properties']['timeseries'][$i]['data']['instant']['details']['ultraviolet_index_clear_sky']). '</td>';
+echo ' <td class="text-dark"> '.$uv. '</td>';
 echo ' <td class="text-dark"> '.$portt.'</td>';
 echo ' <td class="text-dark">'.$sym.'</td>';
 
