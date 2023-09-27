d4718977fc660dbb32741db484bbdb7b744e4a98
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2023-09-27 18:36:16 +0100

    updates

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://test.arranweather.com'),
+    'url' => env('APP_URL', 'http://wx.arranweather.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
