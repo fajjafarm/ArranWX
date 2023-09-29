209c00088a0ddcc684a5c432c79bb448318a8bce
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2023-09-29 19:56:55 +0100

    Update sub-header.blade.php

--- resources/views/components/partials/sub-header.blade.php
+++ resources/views/components/partials/sub-header.blade.php
@@ -4,7 +4,7 @@
     <div class="container-fluid iq-container">
         <div class="row">
             <div class="col-md-12">
-                <div class="d-flex justify-content-between align-items-center flex-wrap">
+                <div class="d-flex justify-content-between align-items-center flex-wrap"><h3>{{ $title }}</h3>
                     <div><?php 
                     $floodstatus ='';
                     $flood = '   <div class="alert alert-success  mt-3" role="alert">
