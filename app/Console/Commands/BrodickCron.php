5b62004459a3353d25e1b9d52d800b40f12ed62d
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2023-10-01 18:46:32 +0100

    Create BrodickCron.php

---
+++ app/Console/Commands/BrodickCron.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+
+class BrodickCron extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:brodick-cron';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+                $path = "https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=55.575&lon=-5.1452&altitude=0";
+        Storage::disk('local')->put('brodick.json', file_get_contents($path));
+  
+        $path = Storage::path('/jsoncron/brodick.json');
+  
+        return response()->download($path);
+    }
+}
