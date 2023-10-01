c01497e06f59977829636aa7c3c6dd1ed9b74679
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2023-10-01 18:52:06 +0100

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-         $schedule->command('brodickcron')->cron('0 */3 * * *);
+         $schedule->command('brodickcron')->cron('0 */3 * * *');
     }
 
     /**
