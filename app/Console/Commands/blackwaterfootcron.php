19357a87b453d87c85d9d3d054f9957c29041d94
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2023-10-01 20:34:14 +0100

    Update blackwaterfootcron.php

--- app/Console/Commands/blackwaterfootcron.php
+++ app/Console/Commands/blackwaterfootcron.php
@@ -6,7 +6,7 @@ use Illuminate\Console\Command;
 //use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\File;
-class BrodickCron extends Command
+class blackwaterfootron extends Command
 {
     /**
      * The name and signature of the console command.
