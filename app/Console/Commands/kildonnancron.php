9e7566d086eed69879183e66a327fbd73063690b
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2023-10-03 09:55:09 +0100

    weather crons

--- app/Console/Commands/kildonnancron.php
+++ app/Console/Commands/kildonnancron.php
@@ -3,7 +3,9 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-
+//use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\File;
 class kildonnancron extends Command
 {
     /**
@@ -11,20 +13,42 @@ class kildonnancron extends Command
      *
      * @var string
      */
-    protected $signature = 'app:kildonnancron';
+    protected $signature = 'kildonnancron';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'kildonnan wx';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
+        
+        $options = array(
+  'http'=>array(
+    'method'=>"GET",
+    'header'=>"Accept-language: en\r\n" .
+              "Cookie: foo=bar\r\n" .  // check function.stream-context-create on php.net
+              "User-Agent: Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.102011-10-16 20:23:10\r\n" // i.e. An iPad 
+  )
+);
+
+$context = stream_context_create($options);
+        
+                $path = "https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=55.4432&lon=-5.1090&altitude=0";
+                $contents = file_get_contents($path, false, $context);
+                
+                if (! Storage::put('kildonnan.json', $contents)) {
+    echo ' The file could not be written to disk...';
+}
+     //  Storage::disk('local')->putFileAs('brodick.json', file_get_contents($path, false, $context));
+ // Storage::disk('local')->putFile('brodick.json', new File(file_get_contents($path, false, $context)), 'public');
+  //      $path = Storage::path('brodick.json');
+  
+        //return response()->download($path);
     }
 }
--- app/Console/Commands/sannoxcron.php
+++ app/Console/Commands/sannoxcron.php
@@ -3,7 +3,9 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-
+//use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\File;
 class sannoxcron extends Command
 {
     /**
@@ -11,20 +13,42 @@ class sannoxcron extends Command
      *
      * @var string
      */
-    protected $signature = 'app:sannoxcron';
+    protected $signature = 'sannoxcron';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'sannox wx';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
+        
+        $options = array(
+  'http'=>array(
+    'method'=>"GET",
+    'header'=>"Accept-language: en\r\n" .
+              "Cookie: foo=bar\r\n" .  // check function.stream-context-create on php.net
+              "User-Agent: Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.102011-10-16 20:23:10\r\n" // i.e. An iPad 
+  )
+);
+
+$context = stream_context_create($options);
+        
+                $path = "https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=55.6624&lon=-5.1601&altitude=8";
+                $contents = file_get_contents($path, false, $context);
+                
+                if (! Storage::put('sannox.json', $contents)) {
+    echo ' The file could not be written to disk...';
+}
+     //  Storage::disk('local')->putFileAs('brodick.json', file_get_contents($path, false, $context));
+ // Storage::disk('local')->putFile('brodick.json', new File(file_get_contents($path, false, $context)), 'public');
+  //      $path = Storage::path('brodick.json');
+  
+        //return response()->download($path);
     }
 }
