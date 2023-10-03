c8d7a342a68b4e4ae264a43601e6780b0965d3e3
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2023-10-03 15:55:56 +0100

    Update quake.blade.php

--- resources/views/wx/quake.blade.php
+++ resources/views/wx/quake.blade.php
@@ -37,7 +37,7 @@ for ($i = 0; $i <= $count; $i++){
   $parts = (explode(":",$data['channel']['item'][$i]['title']));
  
   $location = $parts['2'];
-  $depth =$parts['5'];  
+  $depth =$parts['4'];  
   $location = substr($location, 0, strpos($location, ","));
           
 $mag = str_replace('UK Earthquake alert : ', "", ($data['channel']['item'][$i]['title']))  ;
