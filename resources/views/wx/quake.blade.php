c1d1635115e35684054b9a929c60084bbb235c6f
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2023-10-03 15:05:37 +0100

    quakes

---
+++ resources/views/wx/quake.blade.php
@@ -0,0 +1,60 @@
+<x-app-layout :assets="$assets ?? []" title='Arran Ferry Forecast' :isBanner="true">
+<div class="row">
+   <div class="col-lg-12">
+       <div class="">
+           <div class="card-body">
+                <div class="fancy-table table-responsive rounded">
+                                    <table class="table table-striped mb-0">
+                        <thead>
+                           <tr class="table-dark">
+                               <td >title</td>
+                                <td >description</td>                                                              
+                                   <td >link</td>
+                                       <td >pubdate</td>
+                                          
+                        </thead>
+                        <tbody>
+                       
+                
+                <?php
+        // Read the JSON file 
+        //https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=55.5751&lon=-5.1452&altitude=0
+        //https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=55.5751&lon=-5.1452&altitude=0
+        
+        $json = \File::json('ferryforecast.json');
+//$json = file_get_contents("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=55.5751&lon=-5.1452&altitude=0");
+$json =Storage::get('quake.json');
+
+
+$data = json_decode($json, true);
+$i = 0;
+$count= count($data['item']);
+$count = $count-2;
+
+//echo ($data['properties']['timeseries'][0]['data']['instant']['details']['air_pressure_at_sea_level']);
+
+for ($i = 0; $i <= $count; $i++){
+  
+   
+echo '<tr>';
+echo ' <td>'.($data['item']['title']). '</td>';
+echo ' <td class="text-dark"> '.($data['item']['description'].'</td>';
+echo ' <td class="text-dark">'.($data['item']['link'].'</td>';
+echo ' <td class="text-dark">'.($data['item']['pubDate']. '&degC</td>';
+echo '</tr>';
+     
+                
+                ?>
+                
+                
+                
+
+                                                                              
+                        </tbody>
+                    </table>
+                </div>
+           </div>
+       </div>
+    </div>
+</div>
+</x-app-layout>
