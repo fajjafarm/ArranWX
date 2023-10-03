7d299b854c22c407c56e078b814c376b7f95a7e9
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2023-10-03 15:44:43 +0100

    Update quake.blade.php

--- resources/views/wx/quake.blade.php
+++ resources/views/wx/quake.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout :assets="$assets ?? []" title='Arran Ferry Forecast' :isBanner="true">
+<x-app-layout :assets="$assets ?? []" title='Earthquakes Near Arran' :isBanner="true">
 <div class="row">
    <div class="col-lg-12">
        <div class="">
@@ -34,7 +34,7 @@ $count = $count-2;
 //echo ($data['properties']['timeseries'][0]['data']['instant']['details']['air_pressure_at_sea_level']);
 
 for ($i = 0; $i <= $count; $i++){
-  
+  print_r (explode(";",$data['channel']['item'][$i]['title']));
 $mag = str_replace('UK Earthquake alert : ', "", ($data['channel']['item'][$i]['title']))  ;
 
   
