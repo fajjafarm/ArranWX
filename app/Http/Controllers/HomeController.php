2edbce16fd59ef933fcc0b23e0be6721930cd4aa
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2023-10-06 19:32:55 +0100

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,8 +29,8 @@ class HomeController extends Controller
     }
     public function ferryforecast(Request $request): View
     {
-        $title = 'Arran Ferry Forecast2';
-        $description = 'A rough guide to possible weather realted ferry disruption';
+        $title = "Arran Ferry Forecast2";
+        $description = "A rough guide to possible weather realted ferry disruption";
         $assets = ['chart', 'animation'];
         return view('wx.ferryforecast', compact('title','description','assets'));
     }
