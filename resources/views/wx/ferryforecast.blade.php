4248b9024ade3763378d016e898287bed713ccd8
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2023-10-07 14:34:17 +0100

    Update ferryforecast.blade.php

--- resources/views/wx/ferryforecast.blade.php
+++ resources/views/wx/ferryforecast.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout :assets="$assets ?? []" :title="$title ?? []" :isBanner="true">
 <div class="row">
 <h3>{{ isset($title) ? $title . ' - ' . config('app.name', 'Laravel') : config('app.name', 'Laravel') }}</h3>
-<h5>{{ isset($description) ? $description . ' - ' . config('app.name', 'Laravel') : config('app.name', 'Laravel') }}</h5>
+<h5>{{ title }}</h5>
    <div class="col-lg-12">
        <div class="">
            <div class="card-body">
