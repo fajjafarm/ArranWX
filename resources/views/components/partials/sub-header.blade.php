d7571c0ddda2cadfb0abebec2d149a2920488f73
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2023-10-07 15:23:26 +0100

    Update sub-header.blade.php

--- resources/views/components/partials/sub-header.blade.php
+++ resources/views/components/partials/sub-header.blade.php
@@ -1,8 +1,8 @@
 <div class="iq-navbar-header" style="height: 215px;">
     <div class="container-fluid iq-container">
         <div class="row">
-            <div class="col-md-12"><h3>{{ isset($title) ? $title . ' - ' . config('app.name', 'Laravel') : config('app.name', 'Laravel') }}</h3>
-<h5></h5>
+            <div class="col-md-12"><h3>{{ isset($title) ? $title ) }}</h3>
+<h5>{{ isset($description) ? $description . ' - ' . config('app.name', 'Laravel') : config('app.name', 'Laravel') }}</h5>
                 <div class="d-flex  align-items-center flex-wrap">
                     <div><?php 
                     $floodstatus ='';
