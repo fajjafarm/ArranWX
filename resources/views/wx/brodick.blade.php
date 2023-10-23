51f42b140228c37a22656725959e24e2cec78b04
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2023-10-23 17:58:10 +0100

    Update brodick.blade.php

--- resources/views/wx/brodick.blade.php
+++ resources/views/wx/brodick.blade.php
@@ -35,8 +35,8 @@ $json =Storage::get($location);
 
 $data = json_decode($json, true);
 $i = 0;
-//$count = is_null($data['properties']['timeseries']) ? 0 : count($data['properties']['timeseries']);
-$count= count($data['properties']['timeseries']);
+$count = is_null($data['properties']['timeseries']) ? 0 : count($data['properties']['timeseries']);
+//$count= count($data['properties']['timeseries']);
 $count = $count-2;
 
 //$datar = implode(" ",$data['geometry']['coordinates']);
