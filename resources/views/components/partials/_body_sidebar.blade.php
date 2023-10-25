ac0cd4e6e9edeed43e98238fe8100a2d086d9181
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2023-10-25 22:20:42 +0100

    Update _body_sidebar.blade.php

--- resources/views/components/partials/_body_sidebar.blade.php
+++ resources/views/components/partials/_body_sidebar.blade.php
@@ -2,7 +2,7 @@
     <div class="sidebar-header d-flex align-items-center justify-content-start">
         <a href="{{route('dashboards')}}" class="navbar-brand">
 
-            <h4 class="logo-title d-none d-sm-block" data-setting="app_name">Arran Weather</h4>
+            <h4 class="logo-title >Arran Weather</h4>
         </a>
         <div class="sidebar-toggle" data-toggle="sidebar" data-active="true">
             <i class="icon">
