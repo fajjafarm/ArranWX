f02df6c9ab36ccc2fed8fc45bf281ad76d72cf06
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2023-11-01 19:05:30 +0000

    Update warnings.blade.php

--- resources/views/warnings.blade.php
+++ resources/views/warnings.blade.php
@@ -1,2 +1,11 @@
 <x-app-layout title='Weather Warnings' :assets="$assets ?? []" :isBanner="true">
+
+
+
+
+
+
+https://www.spenergynetworks.co.uk/pages/power_cuts_list.aspx?pCode=KA27
+
+
 </x-app-layout>
