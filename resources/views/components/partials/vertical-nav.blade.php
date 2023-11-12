ed9888dea830db844219089374926cf343d0fa48
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2023-11-12 19:33:26 +0000

    Update vertical-nav.blade.php

--- resources/views/components/partials/vertical-nav.blade.php
+++ resources/views/components/partials/vertical-nav.blade.php
@@ -1,3 +1,4 @@
 <ul class="navbar-nav iq-main-menu " id="sidebar-menu">
     @include(config('laravel-menu.views.bootstrap-items'), ['items' => $HopeUIMenu->roots()])
 </ul>
+Some Pages are still to be completed so don't panic if things look a little wonky
